437abc48ed92082ff4b4bf7f661910eabc963617
Juan Camilo De Los Rios <88950041+C-Rios@users.noreply.github.com>
2023-08-24 08:13:03 -0500

    Update TestCalculator.java

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -2,7 +2,7 @@ import org.earlycareers.CalculatorOperations;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTests {
+public class TestCalculator {
 
     @Test
     void additionAssertion1() {
